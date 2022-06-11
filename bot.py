0693eab10f1def63dfac1260359ed233f17ac731
Dairoku23 <97075691+Dairoku23@users.noreply.github.com>
2022-06-11 13:39:03 -0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,214 @@
+#Stock bot
+#@author Robert Federline
+#@date 6/10/22
+#unspagettied as of 6/10/22
+
+from requests.exceptions import ConnectionError
+import datetime
+import requests
+from bs4 import BeautifulSoup
+import os
+
+os.system("") #makes sure that the ANSI escape sequences work correcly
+
+#ANSI escape sequences for color
+COLOR = {
+    "BLUE": "\033[94m",
+    "GREEN": "\033[92m",
+    "RED": "\033[91m",
+    "ENDC": "\033[0m",
+}
+
+class bot:
+
+    #initializer for bot
+    def __init__(self,stock_amount,stock_held,tic,balance,date):
+        self.stock_amount = stock_amount
+        self.balance = balance
+        self.stock_held = stock_held
+        self.tic = tic
+        
+        #creates files
+        self.title = str(self.tic)+"_"+date+".txt"
+        file = open(self.title, "x")
+        self.titlep = str(self.tic)+"_prices_"+date+".txt"
+        price_file = open(self.titlep, "x")
+        file.close()
+        price_file.close()
+
+    #changes files and clears prices list
+    def day_change(self,date):
+        self.title = str(self.tic)+"_"+date+".txt"
+        file = open(self.title, "x")
+        self.titlep = str(self.tic)+"_prices_"+date+".txt"
+        price_file = open(self.titlep, "x")
+        file.close()
+        price_file.close()
+        self.prices.clear()
+
+    #buy method
+    def buy(self,price):
+        ct = datetime.datetime.now()
+        stock = int(self.balance/price)
+        self.balance=self.balance-stock*price
+        self.stock_held=self.stock_held+stock
+
+        #cancels if there isnt enough money to complete transaction
+        if (self.balance<0.00):
+            self.balance = self.balance + stock*price
+            self.stock_held = self.stock_held - stock
+            if (self.balance>0.00): #may be depreciated
+                stock = int(self.balance/price)
+                self.balance=self.balance-stock*price
+                self.stock_held=self.stock_held+stock
+            print(ct,COLOR["RED"],"| ",self.tic," | Transaction Failed, Insufficent Funds",COLOR["ENDC"])
+            file = open(self.title, "a")
+            file.write(str(ct)+"| Failed buy"+"\n")
+            file.close()
+        else:
+            print(ct,COLOR["RED"],"| Buy ",self.tic," Price:",price," Amount:",stock," Total:",(stock*price),COLOR["ENDC"])
+            text = str(ct)+"| Buy "+self.tic+" Price:"+str(price)+" Amount:"+str(stock)+" Total:"+str(stock*price)
+            file = open(self.title, "a")
+            file.write(text+"\n")
+            file.close()
+
+    #sell method
+    def sell(self,price):
+        ct = datetime.datetime.now()
+        self.balance = self.balance + self.stock_held * price
+        prev_stock_held = self.stock_held
+        self.stock_held = self.stock_held - self.stock_held
+
+        #cancels if there isnt enough stock to complete transaction
+        if (self.stock_held<0):
+            self.balance = self.balance - self.stock_held * price
+            self.stock_held = self.stock_held + self.stock_held
+            print(ct,COLOR["RED"],"| ",self.tic," | Transaction Failed, Insufficent Stock",COLOR["ENDC"])
+            text = (str(ct)+"| Failed Sell")
+            file = open(self.title, "a")
+            file.write(text+"\n")
+            file.close()
+        else:
+            print(ct,COLOR["GREEN"],"| Sell ",self.tic," Price:",price," Amount:",prev_stock_held," Total:",(prev_stock_held*price),COLOR["ENDC"])
+            text = str(ct)+"| Sell "+str(self.tic)+" Price:"+str(price)+" Amount:"+str(prev_stock_held)+" Total:"+str(prev_stock_held*price)
+            file = open(self.title, "a")
+            file.write(text+"\n")
+            file.close()
+
+    #starts the bot, gets inital price
+    def start(self):
+
+        #sets headers and url
+        self.headers = {"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36"}
+        self.url = "https://www.wsj.com/market-data/quotes/"+self.tic.upper()+"/financials"
+
+        try:
+
+            #getting the response from wsj
+            response = requests.get(self.url,headers=self.headers)
+            ct = datetime.datetime.now()
+            print(ct,COLOR["BLUE"],"|",self.tic,"| Response Delay:",response.elapsed," Response Size: ",(len(response.content)/1000),"KB",COLOR["ENDC"])
+            soup = BeautifulSoup(response.text,"lxml")
+            results = soup.find(id = "quote_val")
+            self.prices = [float(results.text)]
+
+        #catches exception of a connection error so that it doesnt terminate program
+        except ConnectionError:
+            ct = datetime.datetime.now()
+            text = str(ct)+"| Connection Timeout"
+            file = open(self.title, "a")
+            file.write(text+"\n")
+            file.close()
+            return str(ct)+COLOR["RED"]+"| Connection Timeout"+COLOR["ENDC"]
+        except requests.exceptions.RequestException:
+            ct = datetime.datetime.now()
+            text = str(ct)+"| Connection Timeout"
+            file = open(self.title, "a")
+            file.write(text+"\n")
+            file.close()
+            return str(ct)+COLOR["RED"]+"| Connection Timeout"+COLOR["ENDC"]
+        except AttributeError:
+            ct = datetime.datetime.now()
+            text = str(ct)+"| Attribute Error"
+            file = open(self.title, "a")
+            file.write(text+"\n")
+            file.close()
+            return str(ct)+COLOR["RED"]+"| Attribute Error"+COLOR["ENDC"]
+
+    #used for updating the bot, checking price and buying or selling if the market situation is right
+    def check_price(self,time_elapsed):
+
+        try:
+
+            #getting the response from wsj
+            response = requests.get(self.url,headers=self.headers)
+            ct = datetime.datetime.now()
+            print(ct,COLOR["BLUE"],"|",self.tic,"| Response Delay:",response.elapsed," Response Size: ",(len(response.content)/1000),"KB",COLOR["ENDC"])
+            soup = BeautifulSoup(response.text,"lxml")
+
+            #finds the price
+            results = soup.find(id = "quote_val")
+            price = float(results.text)
+
+            #writes price to price file
+            price_file = open(self.titlep, "a")
+            price_file.write(str(price)+"\n")
+            price_file.close() 
+
+            #setting up the list the will be used to determine if there is a peak or a low
+            if (len(self.prices)<5):
+                self.prices.append(price)
+            else:
+                self.prices[0] = self.prices[1]
+                self.prices[1] = self.prices[2]
+                self.prices[2] = self.prices[3]
+                self.prices[3] = self.prices[4]
+                self.prices[4] = price               
+
+            #limiter to sell if the stock is after a peak
+            if time_elapsed>(4):
+                if (self.prices[2]>self.prices[0] and self.prices[2]>self.prices[1]):
+                    if (self.prices[2]>self.prices[3] and self.prices[2]>self.prices[4]):
+                        if (self.stock_held>0):
+                            if (price>self.last_buy_price):
+                                self.sell(price)
+                                self.last_sell_price=price
+
+            #limiter to buy if the stock is after a low
+            if time_elapsed>(4):
+                if (self.prices[2]<self.prices[0] and self.prices[2]<self.prices[1]):
+                    if (self.prices[2]<self.prices[3] and self.prices[2]<self.prices[4]):
+                        if self.balance>0.00:
+                            self.buy(price)
+                            self.last_buy_price=price
+            
+            #returns update string and prints update to file
+            ct = datetime.datetime.now()
+            text = str(ct)+"  | "+str(self.tic)+" | min "+str(time_elapsed)+"\tprice:"+str(price)+"\tbalance:"+str(self.balance)+"\tStock Held:"+str(self.stock_held)+"\tMoney in Stocks:"+str(self.stock_held*price)+"\tTotal Balance:"+str((self.stock_held*price)+self.balance)
+            file = open(self.title, "a")
+            file.write(text+"\n")
+            file.close()
+            return text
+        
+        #catches exception of a connection error so that it doesnt terminate program
+        except ConnectionError:
+            ct = datetime.datetime.now()
+            text = str(ct)+"| Connection Timeout"
+            file = open(self.title, "a")
+            file.write(text+"\n")
+            file.close()
+            return str(ct)+COLOR["RED"]+"| Connection Timeout"+COLOR["ENDC"]
+        except requests.exceptions.RequestException:
+            ct = datetime.datetime.now()
+            text = str(ct)+"| Connection Timeout"
+            file = open(self.title, "a")
+            file.write(text+"\n")
+            file.close()
+            return str(ct)+COLOR["RED"]+"| Connection Timeout"+COLOR["ENDC"]
+        except AttributeError:
+            ct = datetime.datetime.now()
+            text = str(ct)+"| Attribute Error"
+            file = open(self.title, "a")
+            file.write(text+"\n")
+            file.close()
+            return str(ct)+COLOR["RED"]+"| Attribute Error"+COLOR["ENDC"]
---
+++ test.py
@@ -0,0 +1,107 @@
+#Test for stock bot
+#@author Robert Federline
+#@date 6/10/22
+#unspagettied as of 6/10/22
+
+import datetime
+import time
+from bot import bot
+import os
+
+os.system("") #makes sure that the ANSI escape sequences work correcly
+
+#ANSI escape sequences for color
+COLOR = {
+    "YELLOW": "\033[93m",
+    "ENDC": "\033[0m",
+}
+
+delay = 1 #delay in minutes, can be changed to 5 if internet is being garbage, though you have to quit and rerun the program
+#click on the terminal, press ctrl+c (you may have to press it multiple times) 
+#change delay & save file, file>save file
+#click on terminal again, press the up arrow to get command to run, then hit enter
+#command to run should be 'python -m test' 
+balance = 500.00 #per stock
+time_elapsed = 0 
+isOpen = False
+x = 1
+
+print("Press CTRL+C to quit")
+
+#getting date
+cd = datetime.datetime.now()
+cd_string = str(cd)
+cd_strings = cd_string.split(" ",1)
+cd_strings_date = cd_strings.pop(0)
+cd = cd_strings_date.split("-",2)
+day_time = cd.pop(2)
+day_time = day_time.split(" ",1)
+previous_day = day_time.pop(0)
+
+#creating bots
+pshg_bot = bot(0,0,"pshg",balance,cd_strings_date)
+bmea_bot = bot(0,0,"bmea",balance,cd_strings_date)
+iova_bot = bot(0,0,"iova",balance,cd_strings_date)
+
+while x==1:
+
+    #extracting hour and minutes from system datetime
+    ct = datetime.datetime.now()
+    ct_string = str(ct)
+    ct_strings = ct_string.split(" ",1)
+    ct_strings_time = ct_strings.pop(1)
+    ct_strings_time_units = ct_strings_time.split(":")
+    hour = ct_strings_time_units.pop(0)
+    minute = ct_strings_time_units.pop(0)
+
+    #checks date and changes file if the day changes
+    date = ct_strings.pop(0)
+    cd_strings = ct_string.split("-",2)
+    day_time = cd_strings.pop(2)
+    day_time = day_time.split(" ",1)
+    day = day_time.pop(0)
+    if (day != previous_day):
+        pshg_bot.day_change(date)
+        bmea_bot.day_change(date)
+        iova_bot.day_change(date)
+    previous_day = day
+
+    #checking if the market is open
+    if (int(hour)<=9 or int(hour)>=16):
+        isOpen = False
+    if (((int(hour)==9 and int(minute)>29) or int(hour)>9) and (int(hour)<16)):
+        isOpen = True
+
+    #start bots when market opens
+    if (int(hour)<=9):
+        if (int(hour)==9):
+            if (int(minute)>29 and int(minute)<(30+delay)):
+                pshg_bot.start()
+                bmea_bot.start()
+                iova_bot.start()
+                time_elapsed = 0
+                isOpen = True
+
+    
+    #if bot is started after the market opens | for testing purposes: and (int(hour)>=10 or (int(hour)<9))
+    if time_elapsed==0:
+        pshg_bot.start()
+        bmea_bot.start()
+        iova_bot.start()
+
+
+    #alerting user to market status
+    if (isOpen==False):
+        print(ct,COLOR["YELLOW"],"| CLOSED | The market opens at 9:30 AM",COLOR["ENDC"])
+    if (isOpen==True):
+        print(ct,COLOR["YELLOW"],"| OPEN | The market closes at 4:00 PM",COLOR["ENDC"])
+
+    #updating bots
+    if isOpen==True:
+        print(pshg_bot.check_price(time_elapsed))
+        print(bmea_bot.check_price(time_elapsed))
+        print(iova_bot.check_price(time_elapsed))
+
+    #delay and updates time
+    time_elapsed = time_elapsed+delay
+    time.sleep(delay*60)
